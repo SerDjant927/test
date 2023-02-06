68e8cb9c87774068748a16f9b0d7b995b891893f
Sergey <sergey.poroshin927@gmail.com>
2023-02-06 20:40:57 +0700

    06/02/2023

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="page" id="app">
     <HeaderBlock></HeaderBlock>
+    <main>
+      <MainBlock></MainBlock>
+    </main>
   </div>
 
 </template>
@@ -8,10 +11,12 @@
 <script>
 
 import HeaderBlock from './components/HeaderBlock.vue'
+import MainBlock from './components/MainBlock.vue'
 export default {
   name: 'App',
   components: {
-    HeaderBlock
+    HeaderBlock,
+    MainBlock
   }
 }
 </script>
